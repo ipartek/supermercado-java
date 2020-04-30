7767841a9e836ca255136e71a47d121a339b3b9a
anderuraga <ander.uraga@gmail.com>
2020-04-30 10:59:29 +0200

    ejercicios de CRUD

---
+++ src/main/java/com/ipartek/formacion/BuscarProductosPorNombre.java
@@ -0,0 +1,10 @@
+package com.ipartek.formacion;
+
+public class BuscarProductosPorNombre {
+	
+	
+	// String sql = "SELECT id, nombre FROM producto WHERE nombre LIKE ? ;";
+	// String nombre = "galle";
+	// pst.setString( 1, "%" + nombre + "%")
+ 
+}
