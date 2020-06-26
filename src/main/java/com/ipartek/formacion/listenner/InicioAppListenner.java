3dceeeb4949867ecfabc53a7da367be93c7add2b
anderuraga <ander.uraga@gmail.com>
2020-06-26 08:57:36 +0200

    libreria para Logs o trazas

--- src/main/java/com/ipartek/formacion/listenner/InicioAppListenner.java
+++ src/main/java/com/ipartek/formacion/listenner/InicioAppListenner.java
@@ -1,9 +1,13 @@
 package com.ipartek.formacion.listenner;
 
+
+
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+
+import org.apache.log4j.Logger;
 
 import com.ipartek.formacion.controller.Alerta;
 import com.ipartek.formacion.modelo.dao.impl.CategoriaDAOImpl;
@@ -15,6 +19,7 @@ import com.ipartek.formacion.modelo.dao.impl.CategoriaDAOImpl;
 @WebListener
 public class InicioAppListenner implements ServletContextListener {
 
+	private final static Logger LOG = Logger.getLogger(InicioAppListenner.class);
 	static private final CategoriaDAOImpl categoriaDAO = CategoriaDAOImpl.getInstance();
 
 	/**
@@ -29,7 +34,7 @@ public class InicioAppListenner implements ServletContextListener {
      */
     public void contextInitialized(ServletContextEvent sce)  { 
          // cuando ejecutamos la App en el Servidor, al arrancar la 1º vez
-    	System.out.println("Estamos arrancado la App, y soy un evento");
+    	LOG.info("Estamos arrancado la App, y soy un evento");
     	
     	
     	// Este contexto es para toda la Aplicacion y es accesible desde cualñquier JSP o Servlet    	
@@ -40,6 +45,7 @@ public class InicioAppListenner implements ServletContextListener {
     		contextoAplicacion.setAttribute("categorias", categoriaDAO.getAll() );
     		
     	}catch (Exception e) {
+    		LOG.fatal(e);
     		contextoAplicacion.setAttribute("alerta", new Alerta("danger", "Tenemos un problema sin determinar") );
 		}	
     	
