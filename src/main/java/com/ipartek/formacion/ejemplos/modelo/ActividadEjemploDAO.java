842309d697c4783aca1781a1006c37442f072473
anderuraga <ander.uraga@gmail.com>
2020-07-07 12:33:34 +0200

    ejemplo de actividad

---
+++ src/main/java/com/ipartek/formacion/ejemplos/modelo/ActividadEjemploDAO.java
@@ -0,0 +1,18 @@
+package com.ipartek.formacion.ejemplos.modelo;
+
+import java.util.List;
+
+import com.ipartek.formacion.modelo.pojo.Producto;
+
+public interface ActividadEjemploDAO {
+
+	
+	List<Producto> listarProductos();
+	
+	List<String> listarCategoriasProductos();
+	
+	
+	
+	
+	
+}
