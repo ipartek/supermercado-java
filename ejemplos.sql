db371e35cfd97e669bfab41a3e3d9ed2ec45d005
anderuraga <ander.uraga@gmail.com>
2020-07-15 11:54:00 +0200

    subconsulta y group by

--- ejemplos.sql
+++ ejemplos.sql
@@ -21,5 +21,21 @@ UPDATE producto SET nombre = 'Iphone 7 Plus' WHERE id = 30;
 DELETE FROM producto WHERE id = 19;
 
 
+/* gruop by y subconsulta */
+
+-- CUIDADO con COUNT(*) porque cuenta los NULL
+SELECT
+  c.id,
+  c.nombre,
+  COUNT(p.id) as 'num_productos',    
+  ( SELECT nombre FROM producto WHERE id_categoria = c.id ORDER BY precio ASC LIMIT 1 ) as 'producto_mas_caro_barato',
+  MIN( p.precio ) as 'producto_mas_barato',  
+  ( SELECT nombre FROM producto WHERE id_categoria = c.id ORDER BY precio DESC LIMIT 1 ) as 'producto_mas_caro_nombre',
+  MAX( p.precio ) as 'producto_mas_caro_precio',
+  ROUND( AVG( p.precio ) , 2 ) as 'producto_media'
+FROM categoria c LEFT JOIN producto p ON c.id = p.id_categoria 
+GROUP BY p.id_categoria ;
+
+
 
 
